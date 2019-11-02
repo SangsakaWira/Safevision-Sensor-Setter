b8ebc2f89a4e30957d33f8efb8db50b70eeb9f79
Sangsaka Wira Utama <sangsakawira@Sangsakas-MacBook-Pro-4.local>
2019-11-02 10:42:24 +0700

    SSID Added

--- app/src/main/java/com/example/safevisionssid/MainActivity.kt
+++ app/src/main/java/com/example/safevisionssid/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.safevisionssid
 
-//import io.github.rybalkinsd.kohttp.ext.httpGet
+import android.content.Context
+import android.net.wifi.WifiConfiguration
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.os.StrictMode
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import io.github.rybalkinsd.kohttp.ext.httpGet
-import java.lang.Exception
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,9 +24,11 @@ class MainActivity : AppCompatActivity() {
         val et_pass:EditText = findViewById(R.id.password) as EditText
         val et_username:EditText = findViewById(R.id.username) as EditText
         val et_host:EditText = findViewById(R.id.host) as EditText
+        val et_sensor:EditText = findViewById(R.id.sensorname) as EditText
         val tv_ssid:TextView = findViewById(R.id.ssid_saved) as TextView
         val tv_pass:TextView = findViewById(R.id.password_saved) as TextView
         val tv_user:TextView = findViewById(R.id.user_saved) as TextView
+//        val tv_sensor:Spinner = findViewById(R.id.sensorname) as Spinner
         val button:Button = findViewById(R.id.submit) as Button
 
         button.setOnClickListener {
@@ -37,6 +36,9 @@ class MainActivity : AppCompatActivity() {
             var pass = et_pass.text.toString()
             var user = et_username.text.toString()
             var host = et_host.text.toString()
+//            var sensor = tv_sensor.text.toString()
+
+            connectWifi("SUMIATI","90908080")
             tv_ssid.setText(ssid)
             tv_pass.setText(pass)
             tv_user.setText(user)
@@ -74,4 +76,33 @@ class MainActivity : AppCompatActivity() {
         println("http://"+host+"/"+"ssid/"+user)
         val response = ("http://"+host+"/"+"username/"+user).httpGet()
     }
+
+    fun connectWifi(networkSSID:String,networkPass:String) {
+
+        val conf = WifiConfiguration()
+        conf.SSID = "\"" + networkSSID + "\""
+        conf.preSharedKey = "\""+ networkPass +"\""
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifiManager.addNetwork(conf)
+
+        val list = wifiManager.configuredNetworks
+        for (i in list) {
+            Log.d("ConnectWifi", i.SSID)
+            if (i.SSID != null && i.SSID == "\"" + networkSSID + "\"") {
+                wifiManager.disconnect()
+                wifiManager.enableNetwork(i.networkId, true)
+                wifiManager.reconnect()
+                break
+            }
+        }
+    }
+
+    fun funCheckStatus(host:String) {
+        val local_response = ("http://"+host+"/"+"getAll/").httpGet()
+        val server_response= ("http://"+host).httpGet()
+        val local_body = local_response.body()
+        val server_body = local_response.body()
+    }
+
 }
+
