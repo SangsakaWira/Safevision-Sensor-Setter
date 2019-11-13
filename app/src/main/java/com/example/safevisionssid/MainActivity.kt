5e6c8193d7762e85121d1f5ff4e6ec7a40389a19
Sangsaka Wira Utama <sangsakawira@Sangsakas-MacBook-Pro-4.local>
2019-11-13 21:20:07 +0700

    update wifi

--- app/src/main/java/com/example/safevisionssid/MainActivity.kt
+++ app/src/main/java/com/example/safevisionssid/MainActivity.kt
@@ -3,15 +3,28 @@ package com.example.safevisionssid
 import android.content.Context
 import android.net.wifi.WifiConfiguration
 import android.net.wifi.WifiManager
-import android.os.Bundle
-import android.os.StrictMode
+import android.os.*
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import io.github.rybalkinsd.kohttp.ext.httpGet
-
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val HOST = "192.168.4.1:3000"
+    }
+
+    val et_ssid by lazy { findViewById<EditText>(R.id.ssid) }
+    val et_pass by lazy { findViewById<EditText>(R.id.password) }
+    val et_username by lazy { findViewById<EditText>(R.id.username) }
+    val et_sensor by lazy { findViewById<EditText>(R.id.sensorname) }
+    val tv_ssid by lazy { findViewById<TextView>(R.id.ssid_saved) }
+    val tv_pass by lazy { findViewById<TextView>(R.id.password_saved) }
+    val tv_user by lazy { findViewById<TextView>(R.id.user_saved) }
+    val button by lazy { findViewById<Button>(R.id.submit) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,68 +33,56 @@ class MainActivity : AppCompatActivity() {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
-        val et_ssid:EditText = findViewById(R.id.ssid) as EditText
-        val et_pass:EditText = findViewById(R.id.password) as EditText
-        val et_username:EditText = findViewById(R.id.username) as EditText
-        val et_host:EditText = findViewById(R.id.host) as EditText
-        val et_sensor:EditText = findViewById(R.id.sensorname) as EditText
-        val tv_ssid:TextView = findViewById(R.id.ssid_saved) as TextView
-        val tv_pass:TextView = findViewById(R.id.password_saved) as TextView
-        val tv_user:TextView = findViewById(R.id.user_saved) as TextView
-//        val tv_sensor:Spinner = findViewById(R.id.sensorname) as Spinner
-        val button:Button = findViewById(R.id.submit) as Button
-
         button.setOnClickListener {
-            var ssid = et_ssid.text.toString()
-            var pass = et_pass.text.toString()
-            var user = et_username.text.toString()
-            var host = et_host.text.toString()
-//            var sensor = tv_sensor.text.toString()
+            val ssid = et_ssid.text.toString()
+            val pass = et_pass.text.toString()
+            val user = et_username.text.toString()
+            val sensor = et_sensor.text.toString()
+            val passwordAP = "safevision"
 
-            connectWifi("SUMIATI","90908080")
-            tv_ssid.setText(ssid)
-            tv_pass.setText(pass)
-            tv_user.setText(user)
-            println("SSID: "+ssid)
-            println("Password: "+pass)
-            println("Username: "+user)
-            println("Host: "+host)
-            try{
-                setSSID(ssid,host)
-                setPassword(pass,host)
-                setUsername(user,host)
-            }
-            catch(e: Exception){
-                val toast = Toast.makeText(applicationContext, "No Connection!", Toast.LENGTH_LONG)
-                toast.show()
+            try {
+                connectWifi(sensor, passwordAP)
+
+                Toast.makeText(applicationContext, "Connected!", Toast.LENGTH_LONG).show()
+
+                checkInfo()
+
+                try {
+                    setSSID(ssid, HOST)
+                    setPassword(pass, HOST)
+                    setUsername(user, HOST)
+                } catch (e: Exception) {
+                    println(e)
+                    Toast.makeText(applicationContext, "Error!", Toast.LENGTH_LONG).show()
+                }
+            } catch (e: Exception) {
+                println(e)
+                Toast.makeText(applicationContext, "No Connection!", Toast.LENGTH_LONG).show()
             }
 
-            val toast = Toast.makeText(applicationContext, "New Data added!", Toast.LENGTH_LONG)
-            toast.show()
         }
     }
 
-    fun setSSID(ssid:String,host:String) {
-        println("http://"+host+"/"+"ssid/"+ssid)
-        val response = ("http://"+host+"/"+"ssid/"+ssid).httpGet()
-        println("")
+    fun setSSID(ssid: String, host: String) {
+        val response = ("http://$host/ssid/$ssid").httpGet()
     }
 
-    fun setPassword(pass:String,host:String) {
-        println("http://"+host+"/"+"ssid/"+pass)
-        val response = ("http://"+host+"/"+"password/"+pass).httpGet()
+    fun setPassword(pass: String, host: String) {
+        val response = ("http://$host/pass/$pass").httpGet()
     }
 
-    fun setUsername(user:String,host:String) {
-        println("http://"+host+"/"+"ssid/"+user)
-        val response = ("http://"+host+"/"+"username/"+user).httpGet()
+    fun setUsername(user: String, host: String) {
+        val response = ("http://$host/userid/$user").httpGet()
     }
 
-    fun connectWifi(networkSSID:String,networkPass:String) {
+    fun reset(host: String) {
+        val response = ("http://$host/restart/").httpGet()
+    }
 
+    fun connectWifi(networkSSID: String, networkPass: String) {
         val conf = WifiConfiguration()
         conf.SSID = "\"" + networkSSID + "\""
-        conf.preSharedKey = "\""+ networkPass +"\""
+        conf.preSharedKey = "\"" + networkPass + "\""
         val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         wifiManager.addNetwork(conf)
 
@@ -97,12 +98,65 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun funCheckStatus(host:String) {
-        val local_response = ("http://"+host+"/"+"getAll/").httpGet()
-        val server_response= ("http://"+host).httpGet()
-        val local_body = local_response.body()
-        val server_body = local_response.body()
+    fun funCheckStatus(host: String): JSONObject {
+        val response = ("http://$host/jsoninfo/").httpGet()
+        return JSONObject(response.body()?.string())
+    }
+
+    fun checkInfo() {
+        println("CHECKING INFO")
+
+        MainProcess {
+            tv_user.text = it?.getString("user_id")
+            tv_pass.text = it?.getString("pass")
+            tv_ssid.text = it?.getString("ssid")
+        }.execute({
+            var counter = 0
+            var result: JSONObject? = null
+            val maxCounter = 30
+
+            while (true) {
+                try {
+                    funCheckStatus(HOST)
+
+                    setSSID(et_ssid.text.toString(), HOST)
+                    setPassword(et_pass.text.toString(), HOST)
+                    setUsername(et_username.text.toString(), HOST)
+
+                    result = funCheckStatus(HOST)
+
+                    println("SUCCESS CHECK INFO")
+
+                    break
+                } catch (e: Exception) {
+                    println("ERROR CHECK INFO")
+                    println(e.message)
+
+                    if (counter > maxCounter) break
+                    counter++
+                    Thread.sleep(1000)
+                }
+            }
+
+            return@execute result
+        })
+    }
+
+    class MainProcess(private val callback: (result: JSONObject?) -> Unit) :
+        AsyncTask<() -> JSONObject?, Unit, JSONObject?>() {
+
+        override fun doInBackground(vararg params: (() -> JSONObject?)): JSONObject? {
+            println("RUNNING CHECK INFO PROCESS")
+            return params[0]()
+        }
+
+        override fun onPostExecute(result: JSONObject?) {
+            callback(result)
+        }
+
     }
 
 }
+
+
 
